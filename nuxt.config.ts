a951d3c7c49b3efafd748a6635f281592e00af3b
nicolaspda <nicolas.pda@hotmail.com>
2024-02-04 02:49:42 -0300

    primeiro commit

---
+++ nuxt.config.ts
@@ -0,0 +1,16 @@
+// https://nuxt.com/docs/api/configuration/nuxt-config
+export default defineNuxtConfig({
+  devtools: { enabled: true },
+  modules: ["nuxt-primevue"],
+  primevue: {
+    options: {
+      ripple: true,
+    },
+  },
+  css: [
+    "primeflex/primeflex.css",
+    "primevue/resources/themes/aura-light-blue/theme.css",
+    "primeicons/primeicons.css",
+    "~/assets/globalStyle.css",
+  ],
+});
