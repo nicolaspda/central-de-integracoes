cff07b3f587cf9fca280a12ecdd20bc693993f28
nicolaspda <nicolas.pda@hotmail.com>
2024-02-11 04:16:34 -0300

    fix:animate Central

--- pages/Central.vue
+++ pages/Central.vue
@@ -12,87 +12,89 @@
     </p>
     <TabView>
       <TabPanel header="Quero Integrar ">
-        <h3>Através do método:</h3>
-        <!-- Primeira Integração - Webhook -->
-        <Panel toggleable collapsed>
-          <template #header>
-            <div class="flex align-items-center gap-2">
-              <Avatar
-                image="https://cdn-icons-png.flaticon.com/128/11896/11896341.png"
-                shape="circle"
-                class="bg-blue-100"
-              />
-              <span class="font-bold">Integração Instantânea</span>
-              <i
-                class="pi pi-question-circle"
-                v-tooltip="
-                  'Adiciona/atualiza um contato na Dinamize quando um evento for disparado do sistema terceiro (Webhook)'
-                "
-                style="font-size: 1rem"
-              ></i>
-            </div>
-          </template>
-          <Webhook />
-        </Panel>
-        <!-- Segunda Integração - Nativa -->
-        <Panel toggleable collapsed class="mt-4">
-          <template #header>
-            <div class="flex align-items-center gap-2">
-              <Avatar
-                icon="pi pi-file-import"
-                shape="circle"
-                class="bg-blue-100"
-              />
-              <span class="font-bold">Integração Nativa </span>
-              <i
-                class="pi pi-question-circle"
-                v-tooltip="
-                  'Permite importar automaticamente os e-mails e seus dados respectivos. Pode ser uma importação única ou recorrente'
-                "
-                style="font-size: 1rem"
-              ></i>
-            </div>
-          </template>
-          <Native />
-        </Panel>
-        <!-- Terceira Integração - Formulário -->
-        <Panel toggleable collapsed class="mt-4">
-          <template #header>
-            <div class="flex align-items-center gap-2">
-              <Avatar icon="pi pi-book" shape="circle" class="bg-blue-100" />
-              <span class="font-bold">Integração via formulário </span>
-              <i
-                class="pi pi-question-circle"
-                v-tooltip="
-                  'Crie formulários nativos ou identifique os formulários do seu site para automaticamente inserir os cadastros na Dinamize'
-                "
-                style="font-size: 1rem"
-              ></i>
-            </div>
-          </template>
-          TAG
-        </Panel>
-        <!-- Quarta Integração - Captura de compra -->
-        <Panel toggleable collapsed class="mt-4">
-          <template #header>
-            <div class="flex align-items-center gap-2">
-              <Avatar
-                icon="pi pi-shopping-cart"
-                shape="circle"
-                class="bg-blue-100"
-              />
-              <span class="font-bold">Integrações para E-commerce </span>
-              <i
-                class="pi pi-question-circle"
-                v-tooltip="
-                  'Integrações específicas para E-commerce. Ex: Capturar dados de compra'
-                "
-                style="font-size: 1rem"
-              ></i>
-            </div>
-          </template>
-          TAG
-        </Panel>
+        <div class="fadein animation-duration-200">
+          <h3>Através do método:</h3>
+          <!-- Primeira Integração - Webhook -->
+          <Panel toggleable collapsed>
+            <template #header>
+              <div class="flex align-items-center gap-2">
+                <Avatar
+                  image="https://cdn-icons-png.flaticon.com/128/11896/11896341.png"
+                  shape="circle"
+                  class="bg-blue-100"
+                />
+                <span class="font-bold">Integração Instantânea</span>
+                <i
+                  class="pi pi-question-circle"
+                  v-tooltip="
+                    'Adiciona/atualiza um contato na Dinamize quando um evento for disparado do sistema terceiro (Webhook)'
+                  "
+                  style="font-size: 1rem"
+                ></i>
+              </div>
+            </template>
+            <Webhook />
+          </Panel>
+          <!-- Segunda Integração - Nativa -->
+          <Panel toggleable collapsed class="mt-4">
+            <template #header>
+              <div class="flex align-items-center gap-2">
+                <Avatar
+                  icon="pi pi-file-import"
+                  shape="circle"
+                  class="bg-blue-100"
+                />
+                <span class="font-bold">Integração Nativa </span>
+                <i
+                  class="pi pi-question-circle"
+                  v-tooltip="
+                    'Permite importar automaticamente os e-mails e seus dados respectivos. Pode ser uma importação única ou recorrente'
+                  "
+                  style="font-size: 1rem"
+                ></i>
+              </div>
+            </template>
+            <Native />
+          </Panel>
+          <!-- Terceira Integração - Formulário -->
+          <Panel toggleable collapsed class="mt-4">
+            <template #header>
+              <div class="flex align-items-center gap-2">
+                <Avatar icon="pi pi-book" shape="circle" class="bg-blue-100" />
+                <span class="font-bold">Integração via formulário </span>
+                <i
+                  class="pi pi-question-circle"
+                  v-tooltip="
+                    'Crie formulários nativos ou identifique os formulários do seu site para automaticamente inserir os cadastros na Dinamize'
+                  "
+                  style="font-size: 1rem"
+                ></i>
+              </div>
+            </template>
+            TAG
+          </Panel>
+          <!-- Quarta Integração - Captura de compra -->
+          <Panel toggleable collapsed class="mt-4">
+            <template #header>
+              <div class="flex align-items-center gap-2">
+                <Avatar
+                  icon="pi pi-shopping-cart"
+                  shape="circle"
+                  class="bg-blue-100"
+                />
+                <span class="font-bold">Integrações para E-commerce </span>
+                <i
+                  class="pi pi-question-circle"
+                  v-tooltip="
+                    'Integrações específicas para E-commerce. Ex: Capturar dados de compra'
+                  "
+                  style="font-size: 1rem"
+                ></i>
+              </div>
+            </template>
+            TAG
+          </Panel>
+        </div>
       </TabPanel>
       <TabPanel header="Integrações Ativas"> <Active /> </TabPanel>
     </TabView>
